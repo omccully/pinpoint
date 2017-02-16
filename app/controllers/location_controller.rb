54fdf30bb7d9622298600399983f400ca184291a
FootKjott <oscarmccully@gmail.com>
2017-02-16 08:12:53 +0000

    create location action

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -1,4 +1,16 @@
 class LocationController < ApplicationController
   def create
+    decoder = NMEAPlus::Decoder.new
+    msg = decoder.parse(params[:nmea])
+  
+    render nothing: true, status: 400 unless msg.checksum_ok?
+
+    @loc = Location.new
+    @loc.latitude = msg.latitude
+    @loc.longitude = msg.longitude
+    @loc.nmea = params[:nmea]
+    @loc.save
+
+    render status: 200
   end
 end
